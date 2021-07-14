76aa90a4e6f2657577192733486e0bf91e5652a7
ElWalto <walterclean@gmail.com>
2021-07-14 10:52:15 -0300

    Subindo arquivos primeiras aulas
    Arquivos criados  nas aulas 01 e 02 do 2º modulo e arquivos do projeto 1ª semana

---
+++ Projeto1/js/humor.js
@@ -0,0 +1,8 @@
+function humor() {
+    document.getElementById("feliz").src = "sp-guitarra";
+
+    document.getElementById("sp-guitarra").src = "sp-pistola";
+
+    document.getElementById("sp-pistola").src = "sp-feliz";
+
+}
