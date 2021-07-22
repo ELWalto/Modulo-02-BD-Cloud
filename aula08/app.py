5512c0ad0889bc74063a71c7e5b5f5a2d155832e
ElWalto <walterclean@gmail.com>
2021-07-21 22:02:33 -0300

    aula08-flask/python

---
+++ aula08/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, redirect,request
+
+app = Flask(__name__)
+itens = list()
+@app.route('/') #Toda rota requer uma função
+def index():
+    return render_template('index.html', titulo='Lista', itens=itens)
+
+@app.route('/new', methods=['POST', 'GET'])
+def new():
+    if request.method =='POST':
+        item = request.form['item']
+        itens.append(item)
+        return redirect('/')
+
+@app.route('/clear')
+def clear():
+    itens.clear()
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
