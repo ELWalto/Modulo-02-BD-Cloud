f089d61faefa8bd7a6e5006e05902717666d061c
ElWalto <walterclean@gmail.com>
2021-08-09 22:55:13 -0300

    Pokedex

--- Portifólio/app.py
+++ Portifólio/app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, redirect, request
-from flask_mail import Mail, Message #Importa o Mail e o Message do flask_mail para facilitar o envio de emails
-
+from flask import Flask, render_template, redirect, request, flash, session
+from flask_mail import Mail, Message #Importa o Mail e o Message do flask_mail para facilitar o envio de emails#
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key = 'Secret'
 
 # Configuração do envio de email.
 mail_settings = {
@@ -17,6 +18,9 @@ mail_settings = {
 app.config.update(mail_settings) #atualizar as configurações do app com o dicionário mail_settings
 mail = Mail(app) # atribuir a class Mail o app atual.
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://nftrzaie:jiLp26A8PKVyn_OsId6gKRoEw3u0Mihc@kesavan.db.elephantsql.com/nftrzaie'
+
+db = SQLAlchemy(app)
 
 #Classe para capturar as informações do formulário de forma mais organizada
 class Contato:
@@ -25,10 +29,105 @@ class Contato:
       self.email = email
       self.mensagem = mensagem
 
+class Projeto(db.Model):
+   id = db.Column (db.Integer, primary_key = True, autoincrement = True)
+   nome = db.Column (db.String(150), nullable = False )
+   imagem = db.Column (db.String(500), nullable = False)
+   descricao = db.Column (db.String(500), nullable = False)
+   link = db.Column(db.String(500), nullable = False)
+   
+   def __init__(self,nome, imagem, descricao, link):
+      self.nome = nome
+      self.imagem = imagem
+      self.descricao = descricao
+      self.link = link
+
+
+
+
+
 # Rota principal apenas para renderizar a página principal.
 @app.route('/')
 def index():
-   return render_template('index.html')
+   session['user_logado'] = None
+   projetos = Projeto.query.all()
+   return render_template('index.html', projetos = projetos)
+
+@app.route('/adm')
+def adm():
+   if 'user_logado' not in session or session['user_logado'] == None:
+      flash('faça o login')
+      return redirect('/login')
+   projetos = Projeto.query.all()
+   return render_template('adm.html', projetos = projetos)
+
+@app.route('/new', methods=['GET', 'POST'])
+def new():
+   if request.method == 'POST': # Verifica se o metodo recebido na requisição é POST
+      # cria o objeto projeto, adiconando os campos do form nele.
+      projeto = Projeto(
+         request.form['nome'],
+         request.form['imagem'],
+         request.form['descricao'],
+         request.form['link']
+      )
+      db.session.add(projeto) # Adiciona o objeto projeto no banco de dados.
+      db.session.commit() # Confirma a operação
+      flash('Projeto criado com sucesso!') # Mensagem de sucesso.
+      return redirect('/adm') # Redireciona para a rota adm
+
+#Crud - Delete#
+@app.route('/delete/<id>') 
+def delete(id):
+   projeto = Projeto.query.get(id) # Busca um projeto no banco através do id
+   db.session.delete(projeto) # Apaga o projeto no banco de dados
+   db.session.commit() # Confirma a operação
+   return redirect('/adm') #Redireciona para a rota ad
+#crud autenticação e login#
+@app.route('/login')
+def login():
+   session['usuario_logado'] = None # Desloga o usuario
+   return render_template('login.html')
+
+
+@app.route('/auth', methods=['GET', 'POST']) # Rota de autenticação
+def auth():
+   if request.form['senha'] == 'admin': # Se a senha for 'admin' faça:
+      session['usuario_logado'] = 'admin' # Adiciona um usuario na sessão
+      flash('Login feito com sucesso!') # Envia mensagem de sucesso
+      return redirect('/adm') # Redireciona para a rota adm
+   else: # Se a senha estiver errada, faça:
+      flash('Erro no login, tente novamente!')  # Envia mensagem de erro
+      return redirect('/login') # Redireciona para a rota login
+
+#Crud- editar
+
+@app.route('/edit/<id>', methods=['GET', 'POST'])
+def edit(id):
+   projeto = Projeto.query.get(id) # Busca um projeto no banco através do id
+   projetos = Projeto.query.all()
+   if request.method == "POST": # Se a requisição for um POST, faça:
+      # Alteração de todos os campos de projetoEdit selecionado no get id
+      projeto.nome = request.form['nome']
+      projeto.descricao = request.form['descricao']
+      projeto.imagem = request.form['imagem']
+      projeto.link = request.form['link']
+      db.session.commit() # Confirma a operação
+      return redirect('/adm') #Redireciona para a rota adm
+   # Renderiza a página adm.html passando o projetoEdit (projeto a ser editado)
+   return render_template('adm.html', projeto=projeto, projetos=projetos) 
+
+#### MODAL ####
+
+
+### Pegar projeto por id ###
+@app.route('/<id>')
+def projeto_por_id(id):
+   projetoDel = Projeto.query.get(id)
+   return render_template('adm.html', projetoDel = projetoDel)
+   
+
+
 
 # Rota de envio de email.
 @app.route('/send', methods=['GET', 'POST'])
@@ -55,4 +154,7 @@ def send():
    return render_template('send.html', formContato = formContato) # Renderiza a página de confirmação de envio.
 
 if __name__ == '__main__':
+   db.create_all()
    app.run(debug=True)
+
+
---
+++ pokedex-flask-front/app.py
@@ -0,0 +1,80 @@
+from flask import Flask, render_template, redirect, request,session
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)  #instanciando objeto 'APP' 
+app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql://sfgworrf:uCpELVO6PpPhWsIpCxcnSfdVTd7iVegU@kesavan.db.elephantsql.com/sfgworrf' #Configurando a URL do Elephant  para o BD SQLAlchemy Lembrando de colocar o QL no postgresql.PARA MUDAR PARA O SQLITE MUDA SÓ A URL 'SQLITE:///POKEDEX.SQLITE3'
+db = SQLAlchemy(app) #DB recebe o aplicativo para o SQLAlchemy
+
+class Pokemon(db.Model): #  a classe POKEMON Virou uma tabela modelo no banco de dados
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True) # Criando uma coluna com ID autoincrement e como chave primaria
+    nome = db.Column(db.String(150), nullable = False)  # Criando uma coluna com nome  String e campo não nulo
+    imagem = db.Column (db.String(500), nullable = False) # Criando uma coluna com nome  String e campo não nulo
+    descricao = db.Column (db.String(500), nullable = False) # Criando uma coluna com nome  String e campo não nulo
+    tipo = db.Column (db.String(150), nullable = False) # Criando uma coluna com nome  String e campo não nulo
+
+    def __init__(self, nome, imagem, descricao, tipo): # Função construtora  passando os parametros das tabelas do banco de dados
+        self.nome = nome
+        self.imagem = imagem
+        self.descricao = descricao
+        self.tipo = tipo
+
+
+@app.route('/') # criação da rota principal
+def index():
+    pokedex = Pokemon.query.all() # Pegando todas linhas dos pokemons em lista 
+    return render_template('index.html',pokedex = pokedex) #rendriza o template do index.html 
+
+@app.route('/add', methods=['GET','POST'])
+def new():
+    if request.method == 'POST': # verificando se o metodo é o post para evitar que o usuário acesse a rota pela url
+        pokemon = Pokemon(request.form['nome'], # criando objeto pokemon da Clase Pokemon
+        request.form['imagem'],
+        request.form['descricao'],
+        request.form['tipo'])
+
+        db.session.add(pokemon) # insert na tabela
+        db.session.commit() # Confirmando a alteração da tabela ### ENTER ###
+        return redirect('/')
+
+@app.route('/edit/<id>', methods=['GET','POST'])
+def edit(id):
+    pokemon = Pokemon.query.get(id) #pokemon recebe uma pesquisa no banco completo pelo id e faz com que o modal de edição apareça
+    pokedex = Pokemon.query.all() # pokedex traz todos os pokemons cadastrados
+    if request.method == 'POST': # verificando se o metodo é post para evitar que o usuário acesse pela url, se verdadeiro ele entra no if do editar e atualiza as informações
+        pokemon.nome = request.form['nome'],
+        pokemon.descricao = request.form['descricao']
+        pokemon.imagem = request.form['imagem']
+        pokemon.tipo = request.form['tipo']
+        db.session.commit() 
+        return redirect('/')
+
+    return render_template('index.html', pokemon = pokemon , pokedex=pokedex) #rendriza o template index.html trazendo as variaveis pokemon e pokedex ja cadastradas no BD
+    
+@app.route('/<id>') # rota para pegar o pokemon por id e habilitar a deleção
+def get_by_id(id):
+    pokemon = Pokemon.query.get(id)
+    pokedex = Pokemon.query.all()
+    return render_template('index.html', pokemonDelete = pokemon , pokedex=pokedex)
+
+@app.route('/delete/<id>') # Rota para deletar efetivamente um pokemon cadastrado pelo id que vem do modal sim 
+def delete(id): 
+    pokemon = Pokemon.query.get(id) # pega o pokemon pelo id
+    db.session.delete(pokemon) #deleta o pokemon
+    db.session.commit() # confirma a deleção
+    return redirect('/') # redireciona para a rota principal
+
+@app.route('/filter',methods=['GET', 'POST'])
+def filter():
+    pokedex = Pokemon.query.filter_by(tipo=request.form['search']).all()
+    return render_template('index.html', pokedex =pokedex)
+
+@app.route('/filter/<param>')
+def filter_by(param):
+    pokedex = Pokemon.query.filter_by(tipo=param).all()
+    return render_template('index.html',pokedex = pokedex)
+
+
+if __name__ == '__main__': 
+    db.create_all() # Verifica se existe a tabela se não existir ele cria .
+    app.run(debug=True) #usando o debug=True Ele atualiza o servidor flask local automaticamente posso alterar a porta usando port = xxx (numero porta)
+
